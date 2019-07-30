5d0a5303cce0fda0095c2de54e1d29e4ec9830c4
Rafael <rafaelbeloduarte@protonmail.com>
2019-07-29 22:41:18 -0300

    A entrada agora é a vazão. Problema a resolver: a aceleração deve ser levada em conta, pois quando há aceleração a vazão varia, como serão dois pistões deve-se encontrar um ponto onde começar a acelerar um enquanto o outro desacelera, mantendo a vazão constante.

--- STEP.ino
+++ STEP.ino
@@ -1,78 +1,138 @@
-void steps(int n_passos, int periodo, int periodo_inicial, int acel) {
+// 2615,87 mm3 -> 9 voltas
+// vazão =
+// fator de conversão de [mL/min] para [mm3/µs] = 1.666666667E-5
+
+void steps(unsigned int n_passos, unsigned int periodo, int periodo_inicial, int acel, float vazao) {
   byte sequencia = 0;
-  int cont = 0;
-  int periodo_set = periodo;
+  unsigned int cont = 0;
+  unsigned int periodo_set;
   periodo = periodo_inicial;
 
-  int n_passos_acel = 9*(periodo_inicial - periodo_set) / acel;
+  float inicio = 0, fim = 0, t = 0;
 
-  int start_decel = n_passos - n_passos_acel;
-  int start_acel = n_passos_acel;
+  //10 revoluções -> 25cm -> 90 voltas no máximo (1 volta = 4610 passos)
+  // volume deslocado máximo = 261 586,68 mm3
+  // encontrar o tempo
 
-  while (cont < n_passos) {
-    if (sequencia == 0) {
-      digitalWrite(pino1, HIGH);
-      digitalWrite(pino2, LOW);
-      digitalWrite(pino3, LOW);
-      digitalWrite(pino4, LOW);
+  float volume = 26158.67; //mm3 (z = 5mm)
+  float tempo = 0;
+  float tempo_decorrido = 0;
+
+  vazao = vazao * 1.666666667E-5;
+  Serial.print("vazão (mm3/µs): ");
+  Serial.println(vazao, 10);
+
+  tempo = volume / vazao;
+
+  Serial.print("tempo_calculado: ");
+  Serial.println(tempo);
+
+  periodo_set = tempo / n_passos;
+
+  Serial.print("periodo_set: ");
+  Serial.println(periodo_set);
+
+  unsigned int n_passos_acel = 9 * (periodo_inicial - periodo_set) / acel;
+  unsigned int n_passos_decel = n_passos - n_passos_acel;
+
+  unsigned int per = 0, t_acelera = 0;
+  // para calcular o tempo se for dado o número de passos:
+  for (int i = 0; i < n_passos_acel / 9; i++) {
+    per = per - acel;
+    //    Serial.print("per: ");
+    //    Serial.println(per);
+    for (int j = 0; j < 8; j++) {
+      t_acelera = t_acelera + per;
     }
-    if (sequencia == 1) {
-      digitalWrite(pino1, HIGH);
-      digitalWrite(pino2, HIGH);
-      digitalWrite(pino3, LOW);
-      digitalWrite(pino4, LOW);
-    }
-    if (sequencia == 2) {
-      digitalWrite(pino1, LOW);
-      digitalWrite(pino2, HIGH);
-      digitalWrite(pino3, LOW);
-      digitalWrite(pino4, LOW);
-    }
-    if (sequencia == 3) {
-      digitalWrite(pino1, LOW);
-      digitalWrite(pino2, HIGH);
-      digitalWrite(pino3, HIGH);
-      digitalWrite(pino4, LOW);
-    }
-    if (sequencia == 4) {
-      digitalWrite(pino1, LOW);
-      digitalWrite(pino2, LOW);
-      digitalWrite(pino3, HIGH);
-      digitalWrite(pino4, LOW);
-    }
-    if (sequencia == 5) {
-      digitalWrite(pino1, LOW);
-      digitalWrite(pino2, LOW);
-      digitalWrite(pino3, HIGH);
-      digitalWrite(pino4, HIGH);
-    }
-    if (sequencia == 6) {
-      digitalWrite(pino1, LOW);
-      digitalWrite(pino2, LOW);
-      digitalWrite(pino3, LOW);
-      digitalWrite(pino4, HIGH);
-    }
-    if (sequencia == 7) {
-      digitalWrite(pino1, HIGH);
-      digitalWrite(pino2, LOW);
-      digitalWrite(pino3, LOW);
-      digitalWrite(pino4, HIGH);
-    }
-    if (sequencia < 8) {
-      sequencia++;
-    }
-    else {
-      sequencia = 0;
-      if (cont < start_acel) {
-        periodo = periodo - acel;
-        Serial.println(periodo);
-      }
-      if (cont >= start_decel) {
-        periodo = periodo + acel;
-        Serial.println(periodo);
-      }
-    }
-    cont++;
-    delayMicroseconds(periodo);
+    t_acelera = t_acelera + per;
+    //    Serial.print("t_acelera: ");
+    //    Serial.println(t_acelera);
   }
+  // obs: meus testes indicam que há uma diferença de mais ou
+  // menos 100 000 µs/volta entre o tempo real e o calculado
+  // creio que este seja o tempo o chip utiliza para executar as instruções
+  //  t_mcu = (n_passos - 2 * n_passos_acel) * periodo_mcu;
+  //  t_calculado = 2 * t_acelera + t_mcu + (n_passos / 4610) * 118000;
+  Serial.print("t_acelera: ");
+  Serial.println(t_acelera);
+
+  inicio = micros();
+  if (periodo_set < 500) {
+    Serial.println("Vazão muito alta!");
+  }
+  else {
+    while (cont < n_passos) {
+      if (sequencia == 0) {
+        digitalWrite(pino1, HIGH);
+        digitalWrite(pino2, LOW);
+        digitalWrite(pino3, LOW);
+        digitalWrite(pino4, LOW);
+      }
+      if (sequencia == 1) {
+        digitalWrite(pino1, HIGH);
+        digitalWrite(pino2, HIGH);
+        digitalWrite(pino3, LOW);
+        digitalWrite(pino4, LOW);
+      }
+      if (sequencia == 2) {
+        digitalWrite(pino1, LOW);
+        digitalWrite(pino2, HIGH);
+        digitalWrite(pino3, LOW);
+        digitalWrite(pino4, LOW);
+      }
+      if (sequencia == 3) {
+        digitalWrite(pino1, LOW);
+        digitalWrite(pino2, HIGH);
+        digitalWrite(pino3, HIGH);
+        digitalWrite(pino4, LOW);
+      }
+      if (sequencia == 4) {
+        digitalWrite(pino1, LOW);
+        digitalWrite(pino2, LOW);
+        digitalWrite(pino3, HIGH);
+        digitalWrite(pino4, LOW);
+      }
+      if (sequencia == 5) {
+        digitalWrite(pino1, LOW);
+        digitalWrite(pino2, LOW);
+        digitalWrite(pino3, HIGH);
+        digitalWrite(pino4, HIGH);
+      }
+      if (sequencia == 6) {
+        digitalWrite(pino1, LOW);
+        digitalWrite(pino2, LOW);
+        digitalWrite(pino3, LOW);
+        digitalWrite(pino4, HIGH);
+      }
+      if (sequencia == 7) {
+        digitalWrite(pino1, HIGH);
+        digitalWrite(pino2, LOW);
+        digitalWrite(pino3, LOW);
+        digitalWrite(pino4, HIGH);
+      }
+      if (sequencia < 8) {
+        sequencia++;
+      }
+      else {
+        sequencia = 0;
+        if (cont < n_passos_acel) {
+          periodo = periodo - acel;
+          //Serial.println(periodo);
+        }
+        if (cont > n_passos_decel) {
+          if (periodo < periodo_inicial) {
+            periodo = periodo + acel;
+            //Serial.println(periodo);
+          }
+        }
+      }
+      cont++;
+      delayMicroseconds(periodo);
+      tempo_decorrido = micros() - inicio;
+    }
+  }
+  fim = micros();
+  t = fim - inicio;
+  Serial.print("tempo_decorrido (µs): ");
+  Serial.println(t);
 }
--- steps.ino
+++ steps.ino
@@ -1,13 +1,24 @@
+// unidade de comprimento = [mm]
+//passos/volta =
+//redução = 9 voltas do motor para 1 revolução
+//1 revolução -> 2.5mm
+// periodo mínimo = 500 microsegundos = 5ms
+// pedir a vazão em [mL/min]
+
+float D = 36.5; //diâmetro
+float z = 2.5;  //avanço do pistão
+
 unsigned int freq = 490;
 int pino1 = 8;
 int pino2 = 9;
 int pino3 = 10;
 int pino4 = 11;
-unsigned long tempo = 10;
-int passos = 4500; // n passos por volta
-int periodo = 1;
-int periodo_inicial = 2000;
-int acel = 10;
+unsigned int passos = 46100; // n passos por volta
+unsigned int periodo = 1000;
+unsigned int periodo_inicial = 2000;
+unsigned int acel = 10;
+float vazao = 0;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,15 +30,18 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("numero de passos");
+  Serial.println("vazão");
   while (Serial.available() == 0);
-  int passos = Serial.parseInt();
-  Serial.println(Serial.available());
-  Serial.println("periodo");
-  while (Serial.available() == 0);
-
-  periodo = Serial.parseInt();
-  steps(passos, periodo, periodo_inicial, acel);
+  int vazao = Serial.parseInt();
+  //  Serial.println("numero de passos");
+  //  while (Serial.available() == 0);
+  //  int passos = Serial.parseInt();
+  //  Serial.println(Serial.available());
+  //  Serial.println("periodo");
+  //  while (Serial.available() == 0);
+  //  periodo = Serial.parseInt();
+  //  steps(passos, periodo, periodo_inicial, acel, vazao);
+  steps(passos, periodo, periodo_inicial, acel, vazao);
   Serial.println("finalizado.");
   digitalWrite(pino1, LOW);
   digitalWrite(pino2, LOW);
